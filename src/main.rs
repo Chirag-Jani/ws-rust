9385340cc4d20a3695bf8fa9cd6d1a9948a27006
Chirag-Jani <>
2024-11-04 18:05:27 +0530

    refactor: storing users wip

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,39 @@
 use futures::{SinkExt, StreamExt};
-use log::{error, info};
-// use std::env;
+use log::error;
+use std::collections::HashMap;
 use std::net::SocketAddr;
+use std::sync::Arc;
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
 use tokio_tungstenite::{accept_async, tungstenite::protocol::Message};
 
-pub mod server1;
+type Clients = Arc<Mutex<HashMap<usize, tokio_tungstenite::WebSocketStream<TcpStream>>>>;
+static mut CLIENT_ID: usize = 0;
 
 #[tokio::main]
 async fn main() {
     // logger thing
     env_logger::init();
 
-    // let addr = env::args()
-    //     .nth(1)
-    //     .unwrap_or_else(|| "127.0.0.1:8080".to_string());
-
-    let addr: SocketAddr = "127.0.0.1:8080"
-        .to_string()
-        .parse()
-        .expect("Invalid Address");
+    let addr: SocketAddr = "0.0.0.0:8080".to_string().parse().expect("Invalid Address");
+    println!("WS thing online at: {}", addr);
 
     // server thing ig
     let listener: TcpListener = TcpListener::bind(&addr)
         .await
         .expect("Failied while Address binding.");
 
-    info!("Listening on: {}", addr);
+    let clients: Clients = Arc::new(Mutex::new(HashMap::new()));
 
     while let Ok((stream, _)) = listener.accept().await {
+        // users clone
+        let clients_clone = Arc::clone(&clients);
         // Spawn a new task for each connection
-        tokio::spawn(handle_connection(stream));
+        tokio::spawn(handle_connection(stream, clients_clone));
     }
 }
 
-async fn handle_connection(stream: TcpStream) {
+async fn handle_connection(stream: TcpStream, clients: Clients) {
     // Accept the WebSocket connection
     let ws_stream = match accept_async(stream).await {
         Ok(ws) => ws,
@@ -43,6 +42,20 @@ async fn handle_connection(stream: TcpStream) {
             return;
         }
     };
+
+    println!("--------------------------- WS STream is: {:?}", ws_stream);
+
+    // Generate a unique client ID
+    let client_id;
+    unsafe {
+        CLIENT_ID += 1;
+        client_id = CLIENT_ID;
+    }
+
+    {
+        let mut client_lock = clients.lock().await;
+        // client_lock.insert(client_id, ws_stream);
+    }
 
     // Split the WebSocket stream into a sender and receiver
     let (mut sender, mut receiver) = ws_stream.split();
@@ -55,14 +68,25 @@ async fn handle_connection(stream: TcpStream) {
             Ok(Message::Text(text)) => {
                 println!("Message is: {:?}", text);
 
-                let response = "Hello from Server.".to_string();
-
-                if let Err(e) = sender.send(Message::Text(response)).await {
+                if let Err(e) = sender.send(Message::Text(text.clone())).await {
                     error!("Error sending message: {}", e);
+                }
+
+                let mut clients_lock = clients.lock().await;
+                for (id, client) in clients_lock.iter_mut() {
+                    if *id != client_id {
+                        let response = format!("Client {}: {}", client_id, text);
+                        if let Err(e) = client.send(Message::Text(response)).await {
+                            error!("Error sending message: {}", e);
+                        }
+                    }
                 }
             }
 
-            Ok(Message::Close(_)) => break,
+            Ok(Message::Close(_)) => {
+                println!("Connection Closed.");
+                break;
+            }
 
             Ok(_) => (),
 
