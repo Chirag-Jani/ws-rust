72895c1cba1479ce8f4a1255fea3aca820cd7f95
Chirag-Jani <>
2024-11-05 18:11:12 +0530

    refactor: message send receive done

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,35 @@ use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::Mutex;
+use tokio::sync::{mpsc::UnboundedSender, Mutex};
 use tokio_tungstenite::{accept_async, tungstenite::protocol::Message};
 
-type Clients = Arc<Mutex<HashMap<usize, tokio_tungstenite::WebSocketStream<TcpStream>>>>;
+type Clients = Arc<Mutex<HashMap<usize, UnboundedSender<Message>>>>;
 static mut CLIENT_ID: usize = 0;
 
 #[tokio::main]
 async fn main() {
-    // logger thing
     env_logger::init();
 
-    let addr: SocketAddr = "0.0.0.0:8080".to_string().parse().expect("Invalid Address");
-    println!("WS thing online at: {}", addr);
+    let addr: SocketAddr = "192.168.0.127:8080"
+        .to_string()
+        .parse()
+        .expect("Invalid Address");
+    println!("WS server online at: {}", addr);
 
-    // server thing ig
     let listener: TcpListener = TcpListener::bind(&addr)
         .await
-        .expect("Failied while Address binding.");
+        .expect("Failed to bind address.");
 
     let clients: Clients = Arc::new(Mutex::new(HashMap::new()));
 
     while let Ok((stream, _)) = listener.accept().await {
-        // users clone
         let clients_clone = Arc::clone(&clients);
-        // Spawn a new task for each connection
         tokio::spawn(handle_connection(stream, clients_clone));
     }
 }
 
 async fn handle_connection(stream: TcpStream, clients: Clients) {
-    // Accept the WebSocket connection
     let ws_stream = match accept_async(stream).await {
         Ok(ws) => ws,
         Err(e) => {
@@ -43,57 +41,59 @@ async fn handle_connection(stream: TcpStream, clients: Clients) {
         }
     };
 
-    println!("--------------------------- WS STream is: {:?}", ws_stream);
-
-    // Generate a unique client ID
     let client_id;
     unsafe {
         CLIENT_ID += 1;
         client_id = CLIENT_ID;
     }
 
+    let (mut sender, mut receiver) = ws_stream.split();
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+
     {
-        let mut client_lock = clients.lock().await;
-        // client_lock.insert(client_id, ws_stream);
+        let mut clients_lock = clients.lock().await;
+        clients_lock.insert(client_id, tx);
     }
 
-    // Split the WebSocket stream into a sender and receiver
-    let (mut sender, mut receiver) = ws_stream.split();
+    tokio::spawn(async move {
+        while let Some(message) = rx.recv().await {
+            if sender.send(message).await.is_err() {
+                error!("Failed to send message to client {}", client_id);
+                break;
+            }
+        }
+    });
 
-    println!("Sender is: {:?}\nReceiver is: {:?}", sender, receiver);
-
-    // Handle incoming messages
     while let Some(msg) = receiver.next().await {
         match msg {
             Ok(Message::Text(text)) => {
-                println!("Message is: {:?}", text);
+                println!("Received message from client {}: {}", client_id, text);
 
-                if let Err(e) = sender.send(Message::Text(text.clone())).await {
-                    error!("Error sending message: {}", e);
-                }
-
-                let mut clients_lock = clients.lock().await;
-                for (id, client) in clients_lock.iter_mut() {
-                    if *id != client_id {
-                        let response = format!("Client {}: {}", client_id, text);
-                        if let Err(e) = client.send(Message::Text(response)).await {
-                            error!("Error sending message: {}", e);
-                        }
+                let clients_lock = clients.lock().await;
+                for (&id, tx) in clients_lock.iter() {
+                    // if id != client_id {
+                    let response = format!("Client {}: {}", client_id, text);
+                    if tx.send(Message::Text(response)).is_err() {
+                        error!("Error sending message to client {}", id);
                     }
+                    // }
                 }
             }
 
             Ok(Message::Close(_)) => {
-                println!("Connection Closed.");
+                println!("Client {} disconnected.", client_id);
                 break;
             }
 
             Ok(_) => (),
 
             Err(e) => {
-                error!("Error processing message: {}", e);
+                error!("Error processing message for client {}: {}", client_id, e);
                 break;
             }
         }
     }
+
+    clients.lock().await.remove(&client_id);
+    println!("Client {} removed.", client_id);
 }
